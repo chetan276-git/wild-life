f37ac6f2176c1a7bdf394aaed8e53d6e459bc751
Chetan Mane <119874815+chetan-fleapo@users.noreply.github.com>
2025-02-08 22:18:52 +0530

    fixed

--- components/home/Destination.tsx
+++ components/home/Destination.tsx
@@ -24,42 +24,34 @@ const HomeDestination = () => {
           backgroundRepeat: 'no-repeat',
           backgroundAttachment: 'fixed',
           backgroundSize: 'cover',
+          backgroundPosition: 'bottom',
         }}
       >
         <div className='container'>
           <h2 className='font-semibold text-3xl lg:text-5xl'>
             Animals you will meet
           </h2>
-          <ul className='flex justify-center flex-wrap w-full lg:max-w-[60%] mx-auto items-center mt-10 font-medium text-lg lg:text-2xl gap-5'>
+          <ul className='flex justify-center flex-wrap w-full lg:max-w-[60%] mx-auto items-center mt-10 font-medium gap-3 lg:gap-5'>
             {[
               'Tiager',
-              '-',
               'Peackock',
-              '-',
               'Elephant',
-              '-',
               'Leopard',
-              '-',
               'Giraffe',
-              '-',
               'Lion',
-              '-',
               'Rabbit',
-              '-',
               'Robin',
-              '-',
               'Hummingbird',
-              '-',
               'Kingfisher',
-              '-',
               'Woodpecker',
-              '-',
               'Columbidae',
-              '-',
               'Eagle',
             ].map((i) => (
               <>
-                <li key={i}>
+                <li
+                  key={i}
+                  className='bg-white/20 backdrop-blur-xl rounded-md py-2 px-4 text-lg '
+                >
                   <h3>{i}</h3>
                 </li>
               </>
